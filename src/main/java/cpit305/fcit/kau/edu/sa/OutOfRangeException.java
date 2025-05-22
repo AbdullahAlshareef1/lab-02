af03aa870a6ed1a3617573b9728b5d1204ea1e6a
Abdullah Alshareef <abc12345mon77@gmail.com>
2025-05-22 18:34:17 +0300

    completed lab-2

--- src/main/java/cpit305/fcit/kau/edu/sa/OutOfRangeException.java
+++ src/main/java/cpit305/fcit/kau/edu/sa/OutOfRangeException.java
@@ -1,5 +1,17 @@
 package cpit305.fcit.kau.edu.sa;
 
-public class OutOfRangeException {
-    
+public class OutOfRangeException extends Exception {
+
+    private final int min;
+    private final int max;
+
+    public OutOfRangeException(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public String getMessage() {
+        return "Error: number must be between " + min + " and " + max + ".";
+    }
 }
--- src/main/java/cpit305/fcit/kau/edu/sa/SquareCalcApp.java
+++ src/main/java/cpit305/fcit/kau/edu/sa/SquareCalcApp.java
@@ -2,14 +2,30 @@ package cpit305.fcit.kau.edu.sa;
 
 import java.util.Scanner;
 
-public class SquareCalcApp
-{
-    // Complete this method but do not change the method name
-    public static int square(int number) {
-        return 0;
-    }
-    public static void main( String[] args )
-    {
+public class SquareCalcApp {
 
+    // Complete this method but do not change the method name
+    public static int square(int number) throws OutOfRangeException {
+        if (number < 0 || number > 100) {
+            throw new OutOfRangeException(0, 100);
+        }
+        return number * number;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its square (0 to 100): ");
+
+        try {
+            int num = Integer.parseInt(input.nextLine());
+            int result = square(num);
+            System.out.println(num + " * " + num + " = " + result);
+        } catch (OutOfRangeException e) {
+            System.err.println(e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid input: Please enter an integer.");
+        } finally {
+            System.out.println("Execution complete.");
+        }
     }
 }
